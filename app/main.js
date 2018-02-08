f1ba3f64cb829735c132c4214d48669acb4ccac1
tianchen <tianchen@landhightech.com>
2018-02-08 14:06:22 +0800

    [dev]-webpack搭建react

--- app/main.js
+++ app/main.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import ReactDom from 'react-dom'
 import Component1 from './components/Component.jsx'
+import ShoppingList from './components/Test.jsx'
 
 ReactDom.render(
-  <Component1 />,
+	<div>
+    <Component1 />
+    <ShoppingList name="Sup" age="20" />
+  </div>,
   document.getElementById('content')
 )
